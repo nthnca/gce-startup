0eda1594b45a868d4002d9e6e093bf66643c1fe6
Nathan Bullock <nathanbullock@gmail.com>
2019-05-05 03:00:31 +0000

    Set and export HOME variable

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,6 +5,9 @@ set -ue
 # Log everything to /var/log/messages, should make it easier to debug any problems.
 exec 1> >(logger -s -t $(basename $0)) 2>&1
 
+# HOME isn't set by default for startup script.
+export HOME=/root
+
 # Don't directly use my repo, clone it and use your own.
 # Using my repo is a significant security and stability risk for you, anything I change
 # will affect you as well.
--- startup-script.sh
+++ startup-script.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# Update Packages
+
+# Set IP
+
+# Set up auto-shutdown
+
 echo 1
 echo 2
 echo 123
