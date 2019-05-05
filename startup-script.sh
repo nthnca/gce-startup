67864ede056ae74a27373de9eb221baef4af91f8
Nathan Bullock <nathanbullock@gmail.com>
2019-05-05 04:01:10 +0000

    Update DNS record with VM IP address

--- startup-script.sh
+++ startup-script.sh
@@ -1,11 +1,35 @@
 #!/bin/bash
 
+set -ue
+
+function get_meta() {
+  curl -s -H "Metadata-Flavor: Google" \
+      "http://metadata.google.internal/computeMetadata/$1"
+}
+
+function get_attr() {
+  get_meta "v1/instance/attributes/$1"
+}
+
+echo "Set IP"
+IP_ADDRESS=`get_meta "v1/instance/network-interfaces/0/access-configs/0/external-ip"`
+ZONE=`get_attr IP_ZONE`
+NAME=`get_attr IP_DNS_ADDRESS`
+PROJECT=`get_attr IP_PROJECT`
+OLDIP=`gcloud dns --project="$PROJECT" record-sets list -z $ZONE | grep $NAME | grep -oE '[^ ]+$'`
+echo "$PROJECT $ZONE $NAME $OLDIP $IP_ADDRESS"
+
+if [ "$OLDIP" != "$IP_ADDRESS" ]; then
+  gcloud dns --project="$PROJECT" record-sets transaction start --zone=$ZONE
+  gcloud dns --project="$PROJECT" record-sets transaction remove $OLDIP --name=$NAME --ttl=5 --type=A --zone=$ZONE
+  gcloud dns --project="$PROJECT" record-sets transaction add $IP_ADDRESS --name=$NAME --ttl=5 --type=A --zone=$ZONE
+  gcloud dns --project="$PROJECT" record-sets transaction execute --zone=$ZONE
+fi
+
 # Update Packages
+# This is slow, we should only do this every week or so.
+echo "APT"
+apt update
+apt upgrade
 
-# Set IP
-
-# Set up auto-shutdown
-
-echo 1
-echo 2
-echo 123
+echo "DONE"
